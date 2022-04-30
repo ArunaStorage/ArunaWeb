a3c2d382c0396f2538695a9c77c84686dfaaade0
anna-brandt <anna.brandt99@web.de>
2022-04-30 16:33:39 +0200

    functions for inputs and buttons

--- src/app/dialogs/notifications/notifications.component.ts
+++ src/app/dialogs/notifications/notifications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
@@ -7,11 +8,37 @@ import { Component, OnInit } from '@angular/core';
 
 export class NotificationsComponent implements OnInit {
 
-  checked: boolean = false;
+  selected_project : any;
+  subresources_checked: boolean = false;
+  sub_from_date: boolean = false;
+  notValid: boolean = true;
+  picked_date: any;
   
-  constructor() { }
+  constructor(
+
+    public apiService: ApiService,
+
+  ) { }
 
   ngOnInit(): void {
   }
 
+  send_subscription(){
+    var new_subscription= {selected_project: this.selected_project, subresources_checked: this.subresources_checked, sub_from_date: this.sub_from_date, picked_date: this.picked_date}
+    console.log("Subscription Object:", new_subscription)
+  }
+
+  isValid(){
+    if(this.selected_project != undefined){
+      if(this.picked_date != undefined || this.sub_from_date == false){
+        this.notValid = false
+      }
+      else{
+        this.notValid = true
+      }
+    }     
+    else{
+      this.notValid = true
+    }  
+  }
 }
