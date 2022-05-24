22b0b624e3c8cadf7ff9d62ab08b775d1787cfc9
anna-brandt <anna.brandt99@web.de>
2022-05-24 16:42:39 +0200

    added tabs and object table in create revision

--- src/app/dialogs/create-revision/create-revision.component.ts
+++ src/app/dialogs/create-revision/create-revision.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatFormFieldControl } from '@angular/material/form-field';
+import { ApiService } from '../../services/api.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { CreateObjectComponent } from '../create-object/create-object.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-create-revision',
@@ -7,9 +13,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRevisionComponent implements OnInit {
 
-  constructor() { }
+  new_revision = {
+    name: "",
+    description:"",
+    id: "",
+    parentRevisionId: "",
+    objects: {add:[], update:[], delete:[]},
+    metadata: {add:[], update:[], delete:[]}
+  }
+
+  objects_table: any
+  displayedColumns: string[]
+  valid = false;
+
+  constructor(
+    
+    public apiService: ApiService,
+    private dialog: MatDialog
+
+  ) { 
+    this.objects_table = new MatTableDataSource(this.apiService.objectGroup.currentRevision.objects)
+    this.displayedColumns=[ "name", "description", "created", "actions"]
+  }
 
   ngOnInit(): void {
   }
+
+  isValid(){
+    if(this.new_revision != null){
+      this.valid = true;
+    }
+    console.log(this.new_revision)
+  }
+
+  createObject() {
+    const dialogRef = this.dialog.open(CreateObjectComponent,
+      {
+        hasBackdrop: true,
+        disableClose: true
+      })
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log("Dialog closed: ", result)
+        this.new_revision.objects.add.push(result)
+        this.objects_table = new MatTableDataSource(this.new_revision.objects.add)
+        this.isValid()
+      } else {
+        console.log("Dialog dismissed")
+      }
+    })
+  }
+  
 
 }
