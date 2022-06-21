1281547e6c19283500460a640c919bf36be9df57
anna-brandt <anna.brandt99@web.de>
2022-06-21 13:04:18 +0200

    basic design objects overview

--- src/app/objects-overview/objects-overview.component.ts
+++ src/app/objects-overview/objects-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-objects-overview',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjectsOverviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  goBack(){
+    this.router.navigate(["/dataset_overview"])
+  }
+
+  newObject(){
+
+  }
 }
